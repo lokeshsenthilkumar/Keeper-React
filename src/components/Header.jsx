db2249c115896529bd16295750ad989b5449ca27
Lokesh <lokeshsenthil492@gmail.com>
2021-04-21 22:37:10 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HighlightRounded} from "@material-ui/icons";
+
+function Header() {
+  return (
+    <header>
+      <h1><HighlightRounded fontSize="large" /> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
